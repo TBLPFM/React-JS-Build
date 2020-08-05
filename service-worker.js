869e996ce35d6c75383072322ffaacf94331e1a1
Ainnop <atwauinnocent@gmail.com>
2020-08-05 14:19:48 +0300

    Build 5-August-2020

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb9eb5a50f3b6117d13d6295152a8430.js"
+  "/precache-manifest.336536230bb4f4c7eef4dfd02efe16d6.js"
 );
 
 self.addEventListener('message', (event) => {
